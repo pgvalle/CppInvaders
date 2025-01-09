5ac859e5baa3d16f2be82e68d217bdb3dca13426
pgvalle <pgvalle.nogueira@gmail.com>
2025-01-09 19:52:24 -0300

    rendering score and credit UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
-#include "pico.h"
+#include <pico.h>
+#include "global.h"
 
 int main() {
     pico_init(1);
 
-    pico_set_title((char *)"CppInvaders");
+    pico_set_title("CppInvaders");
     pico_set_size_internal({ 224, 256 });
     pico_set_size_external({ 448, 512 });
     pico_set_grid(0);
     pico_set_anchor(PICO_LEFT, PICO_TOP);
+    pico_set_font(FONT, 8);
     pico_set_color_clear({ 0x0, 0x0, 0x0, 0x0 });
 
     while (1) {
@@ -34,7 +36,11 @@ int main() {
         
         pico_output_clear();
 
-        // desenho
+        pico_output_draw_text({ 8, 8 }, "YOUR SCORE      HIGH-SCORE");
+        char text[32];
+        sprintf(text, "%06d          %06d", 123, 123);
+        pico_output_draw_text({ 24, 24 }, text);
+        pico_output_draw_text({ 144, 240 }, "CREDIT --");
 
         pico_output_present();
     }
