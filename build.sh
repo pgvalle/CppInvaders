3d07f08c2b126cdaa27cbed1487b8d8e53b610f7
pgvalle <pgvalle.nogueira@gmail.com>
2025-01-09 19:20:58 -0300

    First commit. Figuring out pico.

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir -p deps out
+
+command_exists() {
+    if ! command -v "$1" >/dev/null 2>&1; then
+        echo "$1 is not installed. Please install it and try again."
+        exit 1
+    fi
+}
+
+command_exists git
+
+if [ ! -d "deps/pico-sdl" ]; then
+    git clone https://github.com/fsantanna/pico-sdl deps/pico-sdl
+fi
+
+CFLAGS=`pkg-config --cflags sdl2 SDL2_image SDL2_ttf SDL2_gfx SDL2_mixer`
+LIBS=`pkg-config --libs sdl2 SDL2_image SDL2_ttf SDL2_gfx SDL2_mixer`
+
+cd out
+
+pico_src_files=`find ../deps/pico-sdl/src -type f -name "*.c"`
+gcc -c $pico_src_files $CFLAGS
+
+src_files=`find ../src/ -type f -name "*.cpp"`
+g++ -c $src_files -I../deps/pico-sdl/src $CFLAGS
+
+obj_files=`find ./ -type f -name "*.o"`
+g++ -o ../a.out $obj_files $LIBS
---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include "pico.h"
+
+int main() {
+    pico_init(1);
+
+    pico_set_title((char *)"CppInvaders");
+    pico_set_size_internal({ 224, 256 });
+    pico_set_size_external({ 448, 512 });
+    pico_set_grid(0);
+    pico_set_anchor(PICO_LEFT, PICO_TOP);
+    pico_set_color_clear({ 0x0, 0x0, 0x0, 0x0 });
+
+    while (1) {
+        int delta = 16;
+        bool should_quit = false;
+        while (delta > 0) {
+            int old = SDL_GetTicks();
+            SDL_Event e;
+            pico_input_event_timeout(&e, SDL_ANY, delta);
+            if (e.type == SDL_QUIT) {
+                should_quit = true;
+                break;
+            }
+
+            // processamento de input
+
+            int dt = SDL_GetTicks() - old;
+            delta -= dt;
+        }
+
+        if (should_quit) break;
+
+        // processamento de eventos que dependem do tempo
+        
+        pico_output_clear();
+
+        // desenho
+
+        pico_output_present();
+    }
+
+    pico_init(0);
+
+    return 0;
+}
