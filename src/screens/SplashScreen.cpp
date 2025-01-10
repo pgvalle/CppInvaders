8606cfadd3e5fbe3336b2e313a05719813cb593e
pgvalle <pgvalle.nogueira@gmail.com>
2025-01-10 10:23:56 -0300

    fixed splash screen

--- src/screens/SplashScreen.cpp
+++ src/screens/SplashScreen.cpp
@@ -7,13 +7,13 @@
 #define TIME_WAITING 1.5f
 
 static std::string STRINGS[7] = {
-    "PLAY",
-    "SPACE INVADERS",
-    "*SCORE ADVANCES TABLE*",
-    "=? MYSTERY",
-    "=30 POINTS",
-    "=20 POINTS",
-    "=10 POINTS",
+    " PLAY",
+    " SPACE INVADERS",
+    " *SCORE ADVANCES TABLE*",
+    " =? MYSTERY",
+    " =30 POINTS",
+    " =20 POINTS",
+    " =10 POINTS",
 };
 
 static int STRINGS_XOFF[7] = { 9, 4, 0, 7, 7, 7, 7 };
@@ -22,7 +22,7 @@ static int STRINGS_YOFF[7] = { 0, 3, 7, 9, 11, 13, 15 };
 SplashScreen::SplashScreen() {
     state = WAITING1;
     i = 0;
-    j = 0;
+    j = 1;
     time = 0;
 }
 
@@ -34,22 +34,14 @@ void SplashScreen::draw() {
 
     //ui_draw();
 
-    for (int k = 0; k < i; k++) {
-        std::string &str = STRINGS[k];
-        int xoff = 24 + 8 * STRINGS_XOFF[k];
+    for (int k = 0; k <= i; k++) {
+        std::string str = (k < i ? STRINGS[k] : STRINGS[i].substr(0, j));
+        int xoff = 16 + 8 * STRINGS_XOFF[k];
         int yoff = 64 + 8 * STRINGS_YOFF[k];
         pico_output_draw_text({ xoff, yoff }, (char *)str.c_str());
     }
 
-    if (j > 0) {
-        std::string str = STRINGS[i].substr(0, j);
-        int xoff = 24 + 8 * STRINGS_XOFF[i];
-        int yoff = 64 + 8 * STRINGS_YOFF[i];
-        pico_output_draw_text({ xoff, yoff }, (char *)str.c_str());
-    }
-    
-
-    if (i > 2 && j > 0) {
+    if (i > 2) {
         pico_set_image_crop({ 0, 0, 24, 8 });
         pico_output_draw_image({ 59, 136 }, IMG_UFO);
         pico_set_image_crop({ 0, 0, 8, 8 });
@@ -73,24 +65,25 @@ void SplashScreen::update(float delta) {
         break;
 
     case TYPEWRITING1:
-        if (time >= TYPEWRITING_INTERVAL) {
+        if (time < TYPEWRITING_INTERVAL) {
             time = 0;
             // increment word count
             if (j++ == STRINGS[i].length()) {
                 i++;
-                j = 0;
+                j = 1;
             }
-        }
 
-        if (i == 3) { // i = 2 and j == length str[i]
-            state = WAITING2;
-            time = 0;
+            if (i == 2) {
+                state = WAITING2;
+            }
         }
         break;
 
     case WAITING2:
         if (time >= TIME_WAITING) {
             state = TYPEWRITING2;
+            i = 3;
+            j = 1;
             time = 0;
         }
         break;
@@ -98,17 +91,17 @@ void SplashScreen::update(float delta) {
     case TYPEWRITING2:
         if (time >= TYPEWRITING_INTERVAL) {
             time = 0;
-            // increment word count
+            // increment character iterator over string[i]
             if (j++ == STRINGS[i].length()) {
                 i++;
-                j = 0;
+                j = 1;
             }
-        }
 
-        if (i == 7) { // i = 6 and j == length str[i]
-            i = 6;
-            j = STRINGS[i].length();
-            state = WAITING_KEYPRESS;
+            if (i == 7) {
+                state = WAITING_KEYPRESS;
+                i = 6;
+                j = STRINGS[i].length();
+            }
         }
         break;
 
