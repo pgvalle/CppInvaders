47fae2eba2859ee6ecd3fd4761e13675bd3ce3aa
pgvalle <pgvalle.nogueira@gmail.com>
2025-01-14 11:40:37 -0300

    option to explode shot without image

--- src/entts/Shot.cpp
+++ src/entts/Shot.cpp
@@ -1,9 +1,21 @@
 #include "Shot.h"
 #include "CppInvaders.h"
 
+void GAMESCOPE::Shot::explode(float time) {
+    state = EXPLODING;
+    explosion_time = time;
+    show_explosion_img = true;
+}
+
+void GAMESCOPE::Shot::explode_without_img(float time) {
+    state = EXPLODING;
+    explosion_time = time;
+    show_explosion_img = false;
+}
+
 void GAMESCOPE::Shot::draw() {
     int rx = round(x), ry = round(y);
-    char *explosion_img = (char *)(vy >= 0 ? IMG_EXP3 : IMG_EXP2);
+    const char *explosion_img = (vy >= 0 ? IMG_EXP3 : IMG_EXP2);
 
     switch (state) {
     case ALIVE:
@@ -11,8 +23,10 @@ void GAMESCOPE::Shot::draw() {
         pico_output_draw_rect({ rx, ry, 1, 7 });
         break;
     case EXPLODING:
-        pico_set_image_crop({ 0, 0, 0, 0 });
-        pico_output_draw_image({ rx - 3, ry }, explosion_img);
+        if (show_explosion_img) {
+            pico_set_image_crop({ 0, 0, 0, 0 });
+            pico_output_draw_image({ rx - 3, ry }, explosion_img);
+        }
         break;
     case DEAD:
         break;
@@ -28,8 +42,8 @@ void GAMESCOPE::Shot::update(float delta) {
         }
         break;
     case EXPLODING:
-        explosion_time += delta;
-        if (explosion_time >= 0.3f) {
+        explosion_time -= delta;
+        if (explosion_time <= 0) {
             state = DEAD;
         }
         break;
--- src/entts/Shot.h
+++ src/entts/Shot.h
@@ -10,6 +10,10 @@ struct GAMESCOPE::Shot {
     State state = DEAD;
     float x, y, vy;
     float explosion_time = 0;
+    bool show_explosion_img;
+
+    void explode(float time);
+    void explode_without_img(float time);
 
     void draw();
     void update(float delta);
