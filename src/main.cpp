96f942c9dbe54675b74fe354ea50277f16a99162
pgvalle <pgvalle.nogueira@gmail.com>
2025-02-07 11:47:44 -0300

    main.cpp refactored

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,54 @@
-#include <SDL.h>
+#include "CppInvaders.h"
+#include <ctime>
+
+CppInvaders *cppinv = nullptr;
+
+void init();
+void quit();
+void loop();
 
 int main() {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Quit();
+    init();
+    loop();
+    quit();
     return 0;
+}
+
+void init() {
+    srand(time(nullptr));
+
+    SDL_assert(SDL_Init(SDL_INIT_VIDEO));
+    TTF_Init();
+    IMG_Init(IMG_INIT_PNG);
+    Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 4096);
+
+    cppinv = new CppInvaders;
+}
+
+void quit() {
+    delete cppinv;
+
+    Mix_CloseAudio();
+    TTF_Quit();
+    SDL_Quit();
+}
+
+void loop() {
+    while (!cppinv->should_quit) {
+        int timeout = 16, accum = 0;
+        while (timeout > 0) {
+            int before = SDL_GetTicks();
+
+            SDL_Event event;
+            SDL_WaitEventTimeout(&event, timeout);
+            cppinv->process_event(event);
+
+            int delta = SDL_GetTicks() - before;
+            timeout -= delta;
+            accum += delta;
+        }
+
+        float delta = 0.001f * accum;
+        cppinv->update_and_draw(delta);
+    }
 }
