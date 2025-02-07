f47d211c1c5c3005a4de663f44453ed066e21a78
pgvalle <pgvalle.nogueira@gmail.com>
2025-02-07 11:48:45 -0300

    sdl includes

--- src.old/CppInvaders.h
+++ src.old/CppInvaders.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#include <pico.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL2_gfxPrimitives.h>
 
 #define ROUND(x) ((int)round(x))
 
