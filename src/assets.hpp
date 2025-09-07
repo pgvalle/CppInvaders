c3ca8aa3a0f38cf0e35e53edea4568e4d874f464
pgvalle <pgvalle.nogueira@gmail.com>
2025-09-07 13:33:00 -0300

    aliases to new assets

--- src/assets.hpp
+++ src/assets.hpp
@@ -13,6 +13,10 @@
 #define IMG_EXP3 IMG "exp3.png"
 
 #define SFX "./res/sfx/"
+#define SFX_INVADER_KILLED SFX "invader-killed.wav"
+#define SFX_INVADER_STEP0 SFX "invader-step0.wav"
+#define SFX_INVADER_STEP1 SFX "invader-step1.wav"
+#define SFX_INVADER_STEP2 SFX "invader-step2.wav"
+#define SFX_INVADER_STEP3 SFX "invader-step3.wav"
 #define SFX_SPACESHIP_SHOOT SFX "spaceship-shoot.wav"
 #define SFX_SPACESHIP_KILLED SFX "spaceship-killed.wav"
-#define SFX_INVADER_KILLED SFX "invader-killed.wav"
